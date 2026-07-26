afdb738f693bd7dacf68a8a4605f7c34b0682630
Tirthankar98 <tirthankar.ghosh0910@gmail.com>
2026-07-26 12:03:39 +0530

    Setup clean backend architecture

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,36 @@
 const express = require("express");
+const morgan = require("morgan");
+const logger = require("./config/logger");
+const errorHandler = require("./middlewares/errorHandler");
+const ApiError = require("./utils/ApiError");
 
 const app = express();
 
+//morgan
+app.use(
+    morgan("combined", {
+        stream: {
+            write: (message) => logger.info(message.trim()),
+        },
+    })
+);
+//body parser
 app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+    res.send("Backend is working!");
+});
+app.get("/error", (req, res, next) => {
+    next(new ApiError(404 , "User not found"));
+});
+
+// 404 Middleware
+app.use((req, res, next) => {
+    next(new ApiError(404, `Route ${req.originalUrl} not found`));
+});
+
+//global error handler
+app.use(errorHandler);
 
 module.exports = app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
+console.log(process.env.MONGO_URI);
 
 const app = require("./app");
+const logger = require("./config/logger");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
+const startServer = async () => {
+    await connectDB();
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    logger.info(`Server running on port ${PORT}`);
+    // logger.info("Application started");
+    // logger.warn("This route is deprecated");
+    // logger.error("Database connection failed");
 });
+};
+startServer();
